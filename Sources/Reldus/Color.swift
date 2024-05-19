ca2aa82e71ad2989f21c710e2fa2606df81213fb
Jorge Benjumea <me@intron014.com>
2024-05-19 15:35:43 +0200

    Black! White! Blue?!

---
+++ Sources/Reldus/Color.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum Color {
+    case white
+    case black
+}
+
+extension Color {
+    var opposite: Color {
+        switch self {
+        case .white:
+            return .black
+        case .black:
+            return .white
+        }
+    }
+}
