02e43be1e9ee40fefc36a1f8c6c419f6fb8b6fe1
Jorge Benjumea <me@intron014.com>
2024-05-20 16:05:44 +0200

    Move struct updated

--- Sources/Reldus/Move.swift
+++ Sources/Reldus/Move.swift
@@ -1,17 +1,23 @@
 import Foundation
 
 struct Move {
+    let piece: ChessPiece
     let from: Int
     let to: Int
-    let piece: ChessPiece
     let capturedPiece: ChessPiece?
     let promotion: ChessPiece?
-    
-    init(from: Int, to: Int, piece: ChessPiece, capturedPiece: ChessPiece? = nil, promotion: ChessPiece? = nil) {
+    let isEnPassant: Bool
+    let isCastling: Bool
+    let isCheck: Bool
+
+    init(from: Int, to: Int, piece: ChessPiece, capturedPiece: ChessPiece? = nil, promotion: ChessPiece? = nil, isEnPassant: Bool = false, isCastling: Bool = false, isCheck: Bool = false) {
+        self.piece = piece
         self.from = from
         self.to = to
-        self.piece = piece
         self.capturedPiece = capturedPiece
         self.promotion = promotion
+        self.isEnPassant = isEnPassant
+        self.isCastling = isCastling
+        self.isCheck = isCheck
     }
 }
