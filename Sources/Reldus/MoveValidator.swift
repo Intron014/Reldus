1a4a7a213142c137fb6ad23f4d886a8af26a9e18
Jorge Benjumea <me@intron014.com>
2024-05-20 15:44:46 +0200

    Some stuff here to save

---
+++ Sources/Reldus/MoveValidator.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class MoveValidator {
+    static func isMoveLegal(_ move: Move, on board: ChessBoard) -> Bool {
+        return true
+    }
+}
--- Sources/Reldus/main.swift
+++ Sources/Reldus/main.swift
@@ -1,1 +1,14 @@
-print("Yel")
+import Foundation
+
+
+func testPerft() {
+    let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    let board = ChessBoard(fen: fen)
+    
+    let depth = 4
+    let nodes = MoveGenerator.perft(board: board, depth: depth)
+    
+    print("Perft test for depth \(depth): \(nodes) nodes")
+}
+
+testPerft()
