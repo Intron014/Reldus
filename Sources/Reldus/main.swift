0d75efb53e11681543c1de3ea30b7458f1d7488b
Jorge Benjumea <me@intron014.com>
2024-05-22 11:48:14 +0200

    Testing...

--- Sources/Reldus/main.swift
+++ Sources/Reldus/main.swift
@@ -5,14 +5,19 @@ import Foundation
 let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 let chessBoard = ChessBoard(fen: fen)
 chessBoard.printBoard()
+var mmove = Move(from: 8, to: 16, piece: .whitePawn)
+chessBoard.makeMove(mmove)
+chessBoard.printBoard()
+print(mmove.description)
 print(chessBoard.getFEN())
+
+
 
 func testPerft() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     let board = ChessBoard(fen: fen)
-    
-    let depth = 3
-    let nodes = MoveGenerator.perft(board: board, depth: depth)
+    let depth = 1
+    let nodes = perft(board: board, depth: depth)
     
     print("Perft \(depth) nodes: \(nodes.nodes)")
     print("Captures: \(nodes.captures)")
