ffbe06c1224a19de0219e0738f5f19014d13c88e
Jorge Benjumea <me@intron014.com>
2024-05-24 12:26:25 +0200

    refactor: clean up main

--- Sources/Reldus/main.swift
+++ Sources/Reldus/main.swift
@@ -1,16 +1,4 @@
 import Foundation
 
-// Make a board
-
-// let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-// let chessBoard = ChessBoard(fen: fen)
-// chessBoard.printBoard()
-// var mmove = Move(from: 8, to: 16, piece: .whitePawn)
-// chessBoard.makeMove(mmove)
-// chessBoard.printBoard()
-// print(mmove.description)
-// print(chessBoard.getFEN())
-
-
 let uci = UCI()
 uci.start()
